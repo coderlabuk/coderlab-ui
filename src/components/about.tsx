e69e0529e3f5665b368c90a7e60d342d45008a58
Rob Tucker <rob@coderlab.co.uk>
2019-10-22 21:00:26 +0100

    adds contact form

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,6 +5,14 @@ import { Staff } from './staff'
 interface AboutProps {}
 
 const useStyles = makeStyles(theme => ({
+    h1: {
+        marginTop: theme.spacing(5),
+        marginBottom: theme.spacing(5),
+
+    },
+    h2: {
+        marginBottom: theme.spacing(3),
+    },
     pageContainer: {
         paddingTop: theme.spacing(7),
         paddingBottom: theme.spacing(7),
@@ -19,7 +27,9 @@ export const About: FunctionComponent<AboutProps> = () => {
         <Container className={styles.pageContainer}>
             <Grid container spacing={5} direction="column">
                 <Grid item>
-                    <Typography variant="h3" component="h1" gutterBottom>About CoderLab</Typography>
+                    <Typography className={styles.h1} variant="h3" component="h1" gutterBottom>
+                        About CoderLab
+                    </Typography>
                     <Typography>
                         CoderLab is a software consltancy that delivers high quality bespoke projects  
                         in Machine Learning, Big Data, API development and Web Application development. 
@@ -29,7 +39,9 @@ export const About: FunctionComponent<AboutProps> = () => {
                 </Grid>
 
                 <Grid item>
-                    <Typography variant="h5" component="h3" gutterBottom>Our approach</Typography>
+                    <Typography  className={styles.h2} variant="h5" component="h3" gutterBottom>
+                        Our approach
+                    </Typography>
                     <Typography>
                         Our approach is to focus on delivering achievable projects over medium-sized time scales. 
                         Typically our projects last from 6-12 months and within that time 
@@ -37,7 +49,9 @@ export const About: FunctionComponent<AboutProps> = () => {
                 </Grid>
 
                 <Grid item>
-                    <Typography variant="h5" component="h3" gutterBottom>Our staff</Typography>
+                    <Typography className={styles.h2} variant="h5" component="h3" gutterBottom>
+                        Our staff
+                    </Typography>
                     <Staff />
                 </Grid>
             </Grid>
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,11 +1,96 @@
 import * as React from 'react'
-import { Container } from '@material-ui/core';
+import { Typography, Grid, TextField, Button, makeStyles } from '@material-ui/core';
 
-export const Contact = () => {
+const useStyles = makeStyles(theme => ({
+    container: {
+        padding: theme.spacing(2),
+        width: '100%'
+    },
+    title: {
+        paddingTop: theme.spacing(5),
+        paddingBottom: theme.spacing(5),
+    },
+    form: {
+        maxWidth: '700px',
+        width: "100%",
+    },
+    submit: {
+        marginLeft: 'auto',
+    }
+}))
+
+const sendForm = (e: any) => {
+    e.preventDefault();
+    console.log("Form event submitted")
+    console.log(event)
+}
+
+const Form = () => {
+    const styles = useStyles()
+
     return (
-        <Container>
-            Contact component
-        </Container>
+        <form className={styles.form} noValidate onSubmit={sendForm}>
+            <TextField
+                id="filled-full-width"
+                label="Name"
+                style={{ margin: 8 }}
+                placeholder="Jane Doe"
+                helperText=""
+                fullWidth
+                margin="normal"
+                variant="filled"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+            <TextField
+                required
+                id="filled-full-width"
+                label="Email"
+                style={{ margin: 8 }}
+                placeholder="email@example.com"
+                helperText=""
+                fullWidth
+                margin="normal"
+                variant="filled"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+
+            <TextField
+                id="filled-full-width"
+                multiline={true}
+                rows={8}
+                label="Your query"
+                style={{ margin: 8 }}
+                placeholder=""
+                helperText=""
+                fullWidth
+                margin="normal"
+                variant="filled"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+            <Button className={styles.submit} type="submit">Submit</Button>
+        </form>
+    )
+}
+export const Contact = () => {
+    let styles = useStyles()
+
+    return (
+        <Grid container direction="column" alignItems="center" className={styles.container}>
+            <Grid item>
+                <Typography className={styles.title} variant="h4" component="h1">
+                    Get in touch
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Form />
+            </Grid>
+        </Grid>
     )
 }
 
